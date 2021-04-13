2363342951352d0ceb32ccf1b5abc7582b2c8afb
hugoemanuelpintopaulo <hugoemanuelpintopaulo@gmail.com>
2021-04-13 14:59:19 +0200

    Completed the Album portion

--- SongRestApi/Controllers/V1/DTOS/Requests/AlbumUpdateDTO.cs
+++ SongRestApi/Controllers/V1/DTOS/Requests/AlbumUpdateDTO.cs
@@ -17,7 +17,7 @@ namespace SongRestApi.Controllers.V1.DTOS.Requests
         [MaxLength(100, ErrorMessage = "Album name can't be greater than 100 characters")]
         public string AlbumName { get; set; }
         [Required]
-        [DataType(DataType.Currency)] //even though it's currency need the sanitize service
+        //[DataType(DataType.Currency)] //even though it's currency need the sanitize service
         [RegularExpression(@"^\d+.?\d{0,2}$", ErrorMessage = "Price value can only have decimal of two places")]
         [Range(0, 99999.99, ErrorMessage = "The price can only be 5 digits long")]
         public decimal AlbumPrice { get; set; }
--- SongRestApi/Controllers/V1/DTOS/Responses/AlbumReadDTO.cs
+++ SongRestApi/Controllers/V1/DTOS/Responses/AlbumReadDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,7 +10,9 @@ namespace SongRestApi.Controllers.V1.DTOS.Responses
     {
         //The read needs the id to be accessable because we may use it to tack the specific album we want (avoiding null ref exeptions)
         public int AlbumID { get; set; }
+        
         public string AlbumName { get; set; }
+        
         public decimal AlbumPrice { get; set; }
     }
 }
